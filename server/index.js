c75d84d96407bf7ba32fb8cab69f3d164b4c15c3
lanningbl <47733504+lanningbl@users.noreply.github.com>
2020-09-15 00:59:16 -0400

    prepare to deploy

--- server/index.js
+++ server/index.js
@@ -62,6 +62,14 @@ app.post('/api/form', (req, res) => {
   });
 });
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build'));
+  });
+}
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
