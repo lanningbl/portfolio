a4c7f307d63f8334c680684621ea783bcf91f166
lanningbl <47733504+lanningbl@users.noreply.github.com>
2020-09-15 03:04:19 -0400

    prepare to deploy

--- server/index.js
+++ server/index.js
@@ -63,11 +63,12 @@ app.post('/api/form', (req, res) => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  // app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static('../client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build'));
-  });
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.join(__dirname, '../client/build'));
+  // });
 }
 
 const PORT = process.env.PORT || 8080;
