c5d5677b8ec8f135af5187da9ca6b38f90283825
lanningbl <47733504+lanningbl@users.noreply.github.com>
2020-09-15 03:16:51 -0400

    prepare to deploy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config({ silent: process.env.NODE_ENV === 'production' });
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
 const OAuth2 = google.auth.OAuth2;
@@ -63,11 +64,11 @@ app.post('/api/form', (req, res) => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
   // app.use(express.static('../client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build'));
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   });
 }
 
