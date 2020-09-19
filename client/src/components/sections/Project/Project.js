31507a965de5eb294ac125be6dde5b9a6b9f98a5
lanningbl <47733504+lanningbl@users.noreply.github.com>
2020-09-18 23:41:35 -0400

    Add calendar project

--- client/src/components/sections/Project/Project.js
+++ client/src/components/sections/Project/Project.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaGithub } from 'react-icons/fa';
 
 import './Project.scss';
 import useOnScreen from 'hooks/useOnScreen';
@@ -19,10 +20,31 @@ function Project() {
         className={`projects ${visible ? 'fade-in-left' : 'fade-out-left'}`}
         ref={setRef}
       >
-        <div className='project-card'>Coming soon!</div>
-        <div className='project-card'>2</div>
+        <div className='project-card'>
+          <a
+            href='https://brady-mern-calendar.herokuapp.com'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img
+              className='project-img'
+              src={require('assets/img/calendar-app.png')}
+              alt='calendar app'
+            />
+          </a>
+          <div className='img-label'>
+            <a
+              href='https://github.com/lanningbl/calendar'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FaGithub className='github-icon' />
+            </a>
+          </div>
+        </div>
+        {/* <div className='project-card'>2</div>
         <div className='project-card'>3</div>
-        <div className='project-card'>4</div>
+        <div className='project-card'>4</div> */}
       </div>
     </div>
   );
